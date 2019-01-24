c406445d684b0c97dca8c9b911c4429ab904c04c
chika <chika1086@gmail.com>
2019-01-24 19:17:46 +0900

    add article 13

--- replace_html.rb
+++ replace_html.rb
@@ -1,18 +1,14 @@
+@page_num = 13
 def replace_html_tag(args)
   File.open(args[:write_file_path], "w") do |write_file|
     File.open(args[:read_file_path], "r") do |read_file|
       html = read_file.read
-      page_num = 11
-      write_file.puts(%Q[<%= partial "article_info" %>
-<% @body_id = "article" %>
-<% @title = @article_contents[#{page_num}][0] %>
-<% @posted_date = "投稿日：\#{@article_contents[#{page_num}][1]}" %>
-<% @page_number = "page\#{@article_contents[#{page_num}][2]}" %>
-
+      write_file.puts(%Q[<% @body_id = "article" %>
+<% @page_number = #{@page_num} %>
+<% @description = "" %>
 <div class="text-content">
         ])
-      write_file.write(html.gsub(%r@<h1.*?>.*<@, '<div class="text-title"><h1><%= @title %><')
-                           .gsub(%r@<h2.*?>@, '<div class="text-sub-title"><h2 class="sub-title-border">&nbsp;')
+      write_file.write(html.gsub(%r@<h2.*?>@, '<div class="text-sub-title"><h2 class="sub-title-border">&nbsp;')
                            .gsub(%r@<marquee>@, '<pre class="prettyprint linenums"><code>')
                            .gsub(%r@</h1>@, "</h1></div>")
                            .gsub(%r@</h2>@, "</h2></div>")
@@ -23,6 +19,6 @@ def replace_html_tag(args)
 end
 
 replace_html_tag(
-  :write_file_path => "/Users/chika/Documents/my_site_temp/source/articles/page12.html.erb",
+  :write_file_path => "/Users/chika/Documents/my_site_temp/source/articles/page#{@page_num}.html.erb",
   :read_file_path  => "/Users/chika/Documents/my_site_temp/read.html",
 )
