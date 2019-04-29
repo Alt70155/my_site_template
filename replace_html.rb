5d2b07344a5d15b5ecd324ebd149368a37055f3e
chika <chika1086@gmail.com>
2019-04-29 14:43:56 +0900

    add page18

--- replace_html.rb
+++ replace_html.rb
@@ -14,8 +14,8 @@ def replace_html_tag(args)
       # 文字列の一部を抜き出し、その中から正規表現で探すことを一行ではできないため、
       # 文字列全体から;から;を抜き出し、タグなどのエスケープ文字を置き換えたら変数に入れておく
       # 次で文字全体から;から;を変数の文字に置き換える
-      rewrite_html_escape_char = html.match(/;(\s|\S)*?;/).to_s.gsub!(/<br>/, "").gsub!(/[<>"\/;]/, ";" => "", "<" => "&lt;", ">" => "&gt;", '"' => "&quot;", "/" => "&#047;")
-      write_file.write(html.gsub(/;(\s|\S)*?;/, rewrite_html_escape_char)
+      rewrite_html_escape_char = html.match(/\[(\s|\S)*?\]/).to_s.gsub!(/<br>/, "").gsub!(/[<>"\/\[\]]/, "[" => "", "]" => "", "<" => "&lt;", ">" => "&gt;", '"' => "&quot;", "/" => "&#047;")
+      write_file.write(html.gsub(/\[(\s|\S)*?\]/, rewrite_html_escape_char)
                            .gsub(%r@<h2.*?>@, '<div class="text-sub-title"><h2 class="sub-title-border">&nbsp;')
                            .gsub(%r@<marquee>@,    '<pre class="prettyprint linenums"><code>')
                            .gsub(%r@</h1>@,   "</h1></div>")
