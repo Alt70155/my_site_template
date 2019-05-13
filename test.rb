948de96c546cb6f319e59dda17986a88dffb7f6d
chika <chika1086@gmail.com>
2019-05-13 17:55:29 +0900

    delete index ads & refactor test.rb

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
-class Test
+class FileOperation
   def initialize(args)
     @write_file_path = args[:write_file_path]
     @read_file_path  = args[:read_file_path]
@@ -10,7 +10,8 @@ class Test
     USAGE
   end
 
-  def html_escape(html)
+  # TODO:このメソッド単体で使えるようにしたい
+  def escape_html(html)
     # 文字列の一部を抜き出し、その中から正規表現で探すことを一行ではできないため、
     # 文字列全体から[から]を抜き出し、タグなどのエスケープ文字を置き換えたら変数に入れておく
     # 次で文字全体から[から]を変数の文字に置き換える
@@ -33,7 +34,7 @@ class Test
       File.open(@read_file_path, "r") do |read_file|
         html = read_file.read
         write_file.puts(@head)
-        rewrite_html_escape_char = html_escape(html)
+        rewrite_html_escape_char = escape_html(html)
         write_file.write(html.gsub(/\[(\s|\S)*?\]/, rewrite_html_escape_char)
          .gsub(%r@<h2.*?>@,    %Q[<div class="text-sub-title"><h2 class="sub-title-border">&nbsp;])
          .gsub(%r@<marquee>@,  %Q[<div class="language-tag"><B>▼</B></div><pre class="prettyprint linenums"><code>])
@@ -46,7 +47,8 @@ class Test
   end
 
 end
-Test.new(
+
+FileOperation.new(
   write_file_path: "/Users/chika/Documents/my_site_temp/test.html",
   read_file_path:  "/Users/chika/Documents/my_site_temp/read.html",
 ).replace_html_tag
